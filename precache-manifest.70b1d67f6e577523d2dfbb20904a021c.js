4a58d659e6664df40c92e623473d9c526459619d
navya <navyaha.17@gmail.com>
2022-05-14 16:38:14 +0530

    Updates

---
+++ precache-manifest.70b1d67f6e577523d2dfbb20904a021c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22b922f7551b00460667",
+    "url": "/assignment4/static/css/main.65638a5f.chunk.css"
+  },
+  {
+    "revision": "22b922f7551b00460667",
+    "url": "/assignment4/static/js/main.22b922f7.chunk.js"
+  },
+  {
+    "revision": "0d94da33c7c33f1379ac",
+    "url": "/assignment4/static/js/1.0d94da33.chunk.js"
+  },
+  {
+    "revision": "7b43c3240af795f13bd9",
+    "url": "/assignment4/static/js/runtime~main.7b43c324.js"
+  },
+  {
+    "revision": "e174bbe60dc171c5e0598faa4032b22c",
+    "url": "/assignment4/index.html"
+  }
+];
